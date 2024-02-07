5dcdc609b805c5aa1a59a98017e1a10bf41c619e
Vlone Me <francisntima@gmail.com>
2024-02-07 02:29:54 +0000

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+'''Task 2: Get locale from request
+
+This script defines a Flask application that
+retrieves the locale from the request
+headers and sets it using Flask-Babel.
+It also defines a route ("/") that renders
+an HTML template called "3-index.html".
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    '''Config class
+
+    This class defines configuration parameters for
+    the Flask application.
+    '''
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: Best match locale from the supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app)
+
+
+@app.route('/')
+def index() -> str:
+    '''Default route
+
+    Returns:
+        str: Rendered HTML template.
+    '''
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
